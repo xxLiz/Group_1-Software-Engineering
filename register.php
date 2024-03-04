3c39f06aa9ce5eac494ecf1bba1b374643278c9d
nagamanirayapati <n_rayapati@mail.fhsu.edu>
2024-03-04 12:51:33 -0600

    reuse header and footer

--- register.php
+++ register.php
@@ -1,37 +1,49 @@
 <!DOCTYPE html>
 <html>
+
 <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
     <title>Registration Form</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <link
-			rel="stylesheet"
-			href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-			integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
-			crossorigin="anonymous"
-		/>
+    <script src="https://code.jquery.com/jquery-3.3.1.js"
+        integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous">
+        </script>
+    <script>
+        $(function () {
+            $("#header").load("header.html");
+            $("#footer").load("footer.html");
+        });
+    </script>
 </head>
+
 <body>
+    <div id="header"></div>
     <div class="container">
         <!-- Display session message if it exists -->
         <?php
-    session_start();
-    if (isset($_SESSION['error-message'])) {
-        echo '<p id="errorMessage" class="error">' . $_SESSION['error-message'] . '</p>';
-        // Unset session message variable after displaying it
-        unset($_SESSION['error-message']);
-    }
-    ?>
+        session_start();
+        if (isset($_SESSION['error-message'])) {
+            echo '<p id="errorMessage" class="error">' . $_SESSION['error-message'] . '</p>';
+            // Unset session message variable after displaying it
+            unset($_SESSION['error-message']);
+        }
+        ?>
         <h2>Registration Form</h2>
         <form action="process_registration.php" method="post">
             <div class="row">
                 <div class="col">
                     <label for="firstname">First Name:</label>
-                    <input type="text" id="firstname" name="firstname" required onblur="validateRequiredField('firstname', 'First Name', 'firstnameError');">
+                    <input type="text" id="firstname" name="firstname" required
+                        onblur="validateRequiredField('firstname', 'First Name', 'firstnameError');">
                     <span id="firstnameError" class="error-message"></span>
                 </div>
                 <div class="col">
                     <label for="lastname">Last Name:</label>
-                    <input type="text" id="lastname" name="lastname" onblur="validateRequiredField('lastname', 'Last Name', 'lastnameError');" required>
+                    <input type="text" id="lastname" name="lastname"
+                        onblur="validateRequiredField('lastname', 'Last Name', 'lastnameError');" required>
                     <span id="lastnameError" class="error-message"></span>
                 </div>
             </div>
@@ -56,25 +68,29 @@
 
             <div class="row">
                 <label for="addressline1">Address Line 1:</label>
-                <input type="text" id="addressline1" name="addressline1" onblur="validateRequiredField('addressline1', 'Address Line 1', 'addressline1Error');" required>
+                <input type="text" id="addressline1" name="addressline1"
+                    onblur="validateRequiredField('addressline1', 'Address Line 1', 'addressline1Error');" required>
                 <span id="addressline1Error" class="error-message"></span>
             </div>
 
             <div class="row">
                 <label for="addressline2">Address Line 2:</label>
-                <input type="text" id="addressline2" name="addressline2" onblur="validateRequiredField('addressline2', 'Address Line 2', 'addressline2Error');">
+                <input type="text" id="addressline2" name="addressline2"
+                    onblur="validateRequiredField('addressline2', 'Address Line 2', 'addressline2Error');">
                 <span id="addressline2Error" class="error-message"></span>
             </div>
 
             <div class="row">
                 <div class="col">
                     <label for="city">City:</label>
-                    <input type="text" id="city" name="city" onblur="validateRequiredField('city', 'City', 'cityError');" required>
+                    <input type="text" id="city" name="city"
+                        onblur="validateRequiredField('city', 'City', 'cityError');" required>
                     <span id="cityError" class="error-message"></span>
                 </div>
                 <div class="col">
                     <label for="state">State:</label>
-                    <input type="text" id="state" name="state" onblur="validateRequiredField('state', 'State', 'stateError');" required>
+                    <input type="text" id="state" name="state"
+                        onblur="validateRequiredField('state', 'State', 'stateError');" required>
                     <span id="stateError" class="error-message"></span>
                 </div>
             </div>
@@ -105,8 +121,8 @@
             var phoneNumberError = document.getElementById('mobilenumberError');
             // Check if mobile number is empty
             if (!phoneNumberInput.value.trim()) {
-                 phoneNumberError.textContent = 'Mobile number is required.';
-             }
+                phoneNumberError.textContent = 'Mobile number is required.';
+            }
             else if (!validatePhoneNumber(phoneNumberInput.value)) {
                 phoneNumberError.textContent = 'Please enter a valid 10-digit phone number.';
             } else {
@@ -117,92 +133,94 @@
         // Attach event listener for keyboard input on mobile number field
         document.getElementById('mobilenumber').addEventListener('input', displayPhoneNumberError);
         // Function to validate email format
-function validateEmail(email) {
-    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailPattern.test(email);
-}
+        function validateEmail(email) {
+            var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            return emailPattern.test(email);
+        }
 
-// Function to display email validation error
-function displayEmailError() {
-    var emailInput = document.getElementById('email');
-    var emailError = document.getElementById('emailError');
-    if (!emailInput.value.trim()) {
+        // Function to display email validation error
+        function displayEmailError() {
+            var emailInput = document.getElementById('email');
+            var emailError = document.getElementById('emailError');
+            if (!emailInput.value.trim()) {
                 emailError.textContent = 'Email is required.';
-    }
-    else if (!validateEmail(emailInput.value)) {
-        emailError.textContent = 'Please enter a valid email address.';
-    } else {
-        emailError.textContent = '';
-    }
-}
-
-// Attach event listener for keyboard input on email field
-document.getElementById('email').addEventListener('input', displayEmailError);
-
-// Function to validate password format
-function validatePassword(password) {
-    if (!password) {
-        return "Password is required."; // Display error if password is empty
-    }
-    else if (password.length < 6) {
-        return "Password must be at least 6 characters long.";
-    } else if (password.length > 15) {
-        return "Password must not exceed 15 characters.";
-    }
-    return ""; // Return empty string if password is within the acceptable range
-}
-
-// Function to display password validation error
-function displayPasswordError() {
-    var passwordInput = document.getElementById('password');
-    var passwordError = document.getElementById('passwordError');
-    var errorMessage = validatePassword(passwordInput.value);
-    passwordError.textContent = errorMessage;
-}
-
-// Attach event listener for keyboard input on password field
-document.getElementById('password').addEventListener('input', displayPasswordError);
-
-// Function to validate zip code format
-function validateZipCode(zipcode) {
-    var zipCodePattern = /^\d{5}(?:[-\s]\d{4})?$/;
-    return zipCodePattern.test(zipcode);
-}
-
-// Function to display zip code validation error
-function displayZipCodeError() {
-    var zipCodeInput = document.getElementById('zipcode');
-    var zipCodeError = document.getElementById('zipcodeError');
-    if (!zipCodeInput.value.trim()) {
-        zipCodeError.textContent = 'zipcode is required.';
-     }
-    else if (!validateZipCode(zipCodeInput.value)) {
-        zipCodeError.textContent = 'Please enter a valid zip code.';
-    } else {
-        zipCodeError.textContent = '';
-    }
-}
-
-// Attach event listener for keyboard input on zip code field
-document.getElementById('zipcode').addEventListener('input', displayZipCodeError);
-
-//Function is to Validates if a required input field is empty.
-function validateRequiredField(fieldId, fieldName, errorId) {
-        var fieldInput = document.getElementById(fieldId);
-        var errorElement = document.getElementById(errorId);
-        if (!fieldInput.value.trim()) {
-            errorElement.textContent = fieldName + ' is required.';
-        } else {
-            errorElement.textContent = '';
+            }
+            else if (!validateEmail(emailInput.value)) {
+                emailError.textContent = 'Please enter a valid email address.';
+            } else {
+                emailError.textContent = '';
+            }
         }
-    }
-    // Automatically hide error message after 5 seconds
-    setTimeout(function() {
-        var errorMessage = document.getElementById('errorMessage');
-        if (errorMessage) {
-            errorMessage.style.display = 'none';
+
+        // Attach event listener for keyboard input on email field
+        document.getElementById('email').addEventListener('input', displayEmailError);
+
+        // Function to validate password format
+        function validatePassword(password) {
+            if (!password) {
+                return "Password is required."; // Display error if password is empty
+            }
+            else if (password.length < 6) {
+                return "Password must be at least 6 characters long.";
+            } else if (password.length > 15) {
+                return "Password must not exceed 15 characters.";
+            }
+            return ""; // Return empty string if password is within the acceptable range
         }
-    }, 5000); // 5000 milliseconds = 5 seconds
+
+        // Function to display password validation error
+        function displayPasswordError() {
+            var passwordInput = document.getElementById('password');
+            var passwordError = document.getElementById('passwordError');
+            var errorMessage = validatePassword(passwordInput.value);
+            passwordError.textContent = errorMessage;
+        }
+
+        // Attach event listener for keyboard input on password field
+        document.getElementById('password').addEventListener('input', displayPasswordError);
+
+        // Function to validate zip code format
+        function validateZipCode(zipcode) {
+            var zipCodePattern = /^\d{5}(?:[-\s]\d{4})?$/;
+            return zipCodePattern.test(zipcode);
+        }
+
+        // Function to display zip code validation error
+        function displayZipCodeError() {
+            var zipCodeInput = document.getElementById('zipcode');
+            var zipCodeError = document.getElementById('zipcodeError');
+            if (!zipCodeInput.value.trim()) {
+                zipCodeError.textContent = 'zipcode is required.';
+            }
+            else if (!validateZipCode(zipCodeInput.value)) {
+                zipCodeError.textContent = 'Please enter a valid zip code.';
+            } else {
+                zipCodeError.textContent = '';
+            }
+        }
+
+        // Attach event listener for keyboard input on zip code field
+        document.getElementById('zipcode').addEventListener('input', displayZipCodeError);
+
+        //Function is to Validates if a required input field is empty.
+        function validateRequiredField(fieldId, fieldName, errorId) {
+            var fieldInput = document.getElementById(fieldId);
+            var errorElement = document.getElementById(errorId);
+            if (!fieldInput.value.trim()) {
+                errorElement.textContent = fieldName + ' is required.';
+            } else {
+                errorElement.textContent = '';
+            }
+        }
+        // Automatically hide error message after 5 seconds
+        setTimeout(function () {
+            var errorMessage = document.getElementById('errorMessage');
+            if (errorMessage) {
+                errorMessage.style.display = 'none';
+            }
+        }, 5000); // 5000 milliseconds = 5 seconds
     </script>
+    <div id="footer"></div>
 </body>
+
 </html>
