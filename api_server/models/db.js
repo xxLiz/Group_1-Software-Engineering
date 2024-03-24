fc396a575d63dd0c8ced3e4e04bc8fbf0b369da5
kbotova <k_botova@mail.fhsu.edu>
2024-03-24 18:05:52 +0300

    layout changes

--- api_server/models/db.js
+++ api_server/models/db.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 
 const getClient = async () => {
   const client = new Client({
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
+    host: 'dpg-cnj0c7njbltc73c7itj0-a.oregon-postgres.render.com',
+    port: '5432',
+    user: 'csci841_f2024_db_1_user',
+    password: 'nAfCxVtD0GMcUg1xKdvJhDrcDSS0UFhO',
+    database: 'csci841_f2024_db_1',
     ssl: true,
   });
   await client.connect();
