1620acace960136a37ca3df32c39ae4691c50c6e
nagamanirayapati <n_rayapati@mail.fhsu.edu>
2024-04-03 23:24:09 -0500

    use env variables for db config

--- db_config.php
+++ db_config.php
@@ -1,10 +1,15 @@
 <?php
+require __DIR__.'/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 // Database configuration
-$servername = "localhost";
-$port = 3316;
-$username = "root";
-$password = "";
-$dbname = "the-food-depot";
+$servername = $_ENV['DB_HOST'];
+$port = $_ENV['DB_PORT'];
+$username = $_ENV['DB_USER_NAME'];
+$password = $_ENV['DB_PASSWORD'];
+$dbname = $_ENV['DB'];
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
