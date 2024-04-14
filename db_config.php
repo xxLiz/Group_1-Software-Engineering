728845943fe401044cc3e85b1b6859eade86036f
nagamanirayapati <157265348+nagamanirayapati@users.noreply.github.com>
2024-04-14 00:39:28 -0500

    Use classes for registration and login (#12)

--- db_config.php
+++ db_config.php
@@ -1,54 +1,34 @@
 <?php
 require __DIR__.'/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+class DatabaseConnection {
+    private $conn;
+    public function __construct() {
+        $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
+        $dotenv->load();
 
-// Database configuration
-$servername = $_ENV['DB_HOST'];
-$port = $_ENV['DB_PORT'];
-$username = $_ENV['DB_USER_NAME'];
-$password = $_ENV['DB_PASSWORD'];
-$dbname = $_ENV['DB'];
+        // Database configuration
+        $servername = $_ENV['DB_HOST'];
+        $port = $_ENV['DB_PORT'];
+        $username = $_ENV['DB_USER_NAME'];
+        $password = $_ENV['DB_PASSWORD'];
+        $dbname = $_ENV['DB'];
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+        // Create connection
+        $this->conn = new \mysqli($servername, $username, $password, $dbname);
 
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-// Function to insert user data and address data into database
-function insertUserData($firstname, $lastname, $mobilenumber, $email, $password, $addressline1, $addressline2, $city, $state, $zipcode) {
-    global $conn;
-    // Check if the address already exists
-    $existing_address_sql = "SELECT id FROM Address WHERE addressline1 = '$addressline1' AND addressline2 = '$addressline2' AND city = '$city' AND state = '$state' AND zipcode = '$zipcode'";
-    $existing_address_result = $conn->query($existing_address_sql);
-
-    if ($existing_address_result->num_rows > 0) {
-        // Address already exists, retrieve the address ID
-        $row = $existing_address_result->fetch_assoc();
-        $address_id = $row['id'];
-    }
-    else {
-    // Address doesn't exist, insert address data into address_table
-    $address_sql = "INSERT INTO Address (addressline1, addressline2, city, state, zipcode)
-                    VALUES ('$addressline1', '$addressline2', '$city', '$state', '$zipcode')";
-    if ($conn->query($address_sql) === TRUE) {
-        $address_id = $conn->insert_id;
-    } else {
-            return "Error inserting address data: " . $conn->error;
+        // Check connection
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
         }
     }
 
-        // Insert user data into your_table_name and link it to the address
-        $user_sql = "INSERT INTO Users (firstname, lastname, mobilenumber, email, password, address_id)
-                     VALUES ('$firstname', '$lastname', '$mobilenumber', '$email', '$password', $address_id)";
+    public function getConnection() {
+        return $this->conn;
+    }
 
-        if ($conn->query($user_sql) === TRUE) {
-            return true;
-        } else {
-            return "Error inserting user data: " . $conn->error;
-        }
+    public function closeConnection() {
+        $this->conn->close();
+    }
 }
+
--- process_login.php
+++ process_login.php
@@ -1,50 +1,46 @@
 <?php 
 session_start(); 
-include 'db_config.php';
+require 'db_operations.php';
+require 'validations.php';
 
-if (isset($_POST['email']) && isset($_POST['password'])) {
-
-	function validate($data){
-       $data = trim($data);
-	   $data = stripslashes($data);
-	   $data = htmlspecialchars($data);
-	   return $data;
-	}
-
-	$email = validate($_POST['email']);
-	$password = validate($_POST['password']);
-
-	
-		// hashing the password
-        $password = md5($password);
-
-        
-		$sql = "SELECT * FROM Users WHERE email='$email'";
-
-		$result = mysqli_query($conn, $sql);
-
-		if (mysqli_num_rows($result) === 1) {
-			$row = mysqli_fetch_assoc($result);
-			echo $row;
-            if ($row['email'] === $email && $row['password'] === $password) {
-            	$_SESSION['email'] = $row['email'];
-            	$_SESSION['firstname'] = $row['firstname'];
-            	$_SESSION['id'] = $row['id'];
-				header("Location: home.html");
-		        exit();
-            }else{
-				$_SESSION['error-message'] = "Incorect password";
-                 header("Location: login.php");
-		        exit();
+class Process_login{
+	private $dboperations;
+	public function __construct() {
+        $this->dboperations = new DbOperations();
+    }
+	public function loginUser(){
+		if (isset($_POST['email']) && isset($_POST['password'])) {
+			$email = validate($_POST['email']);
+			$password = validate($_POST['password']);
+			// hashing the password
+			$password = md5($password);
+			$user = $this->dboperations->loginUser($email,$password);
+			if ($user!==false) {
+				if ($user['email'] === $email && $user['password'] === $password) {
+					$_SESSION['email'] = $user['email'];
+					$_SESSION['firstname'] = $user['firstname'];
+					$_SESSION['id'] = $user['id'];
+					header("Location: home.html");
+					exit();
+				} else {
+					$_SESSION['error-message'] = "Incorect password";
+					header("Location: login.php");
+					exit();
+				}
 			}
-		}else{
-			$_SESSION['error-message'] = "This email is not registered";
-             header("Location: login.php");
-	        exit();
+			else{
+				$_SESSION['error-message'] = "This email is not registered";
+				header("Location: login.php");
+				exit();
+			}
 		}
-	
-	
-}else{
-	header("Location: login.php");
-	exit();
+		else{
+			header("Location: login.php");
+			exit();
+		}
+	}
 }
+$processlogin=new Process_login();
+$processlogin->loginUser();
+unset($_SESSION['error-message']);
+unset($_SESSION['success-message']);
