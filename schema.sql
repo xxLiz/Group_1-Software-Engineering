2e19d4fbc962670c2b993cf2c8db5c502ab4743b
nagamanirayapati <n_rayapati@mail.fhsu.edu>
2024-04-26 15:59:55 -0500

    Change phone column datatype to VARCHAR in cash_on_delivery table

--- schema.sql
+++ schema.sql
@@ -76,6 +76,6 @@ CREATE TABLE cash_on_delivery (
     name VARCHAR(255) NOT NULL,
     address VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    phone INT(11) NOT NULL,
+    phone VARCHAR(20) NOT NULL
     
 );
