0ba47ecffa6df9368ff1b6103913a49d6f811552
kbotova <89753557+kbotova@users.noreply.github.com>
2024-04-27 13:31:16 -0500

    Create Profile_edit_form.php

---
+++ Profile_edit_form.php
@@ -0,0 +1,14 @@
+<form action="Profile_update.php" method="post">   
+
+   firstname: <input type="text" name="firstname"><br>
+   lastname: <input type="text" name="lastname"><br>
+   email: <input type="email" name="email"><br> 
+   addressline1: <input type="text" name="addressline1"><br>
+   addressline2: <input type="text" name="addressline2"><br>
+   city: <input type="text" name="city"><br>
+   state: <input type="text" name="state"><br>
+   zipcode: <input type="text" name="zipcode"><br>
+   
+   <input type="submit" name="edit">
+   
+</form>
