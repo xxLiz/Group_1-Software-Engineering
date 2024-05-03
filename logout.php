06c942b46fe77f59f8ebb2f015df1ecbafd4ca02
kbotova <89753557+kbotova@users.noreply.github.com>
2024-05-02 23:08:15 -0500

    Update and rename Logout.php to logout.php

---
+++ logout.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <script src="https://code.jquery.com/jquery-3.3.1.js"
+        integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous">
+        </script>
+    <script>
+        $(function () {
+            $("#header").load("header.html");
+            $("#footer").load("footer.html");
+        });
+    </script>
+</head>
+
+<body class="body__style">
+    <div id="header"></div>
+    <div class="container">
+
+		<?php
+		require "DatabaseConnection.php";
+		session_start();
+	
+		$_SESSION = array();
+	
+		session_destroy();
+
+		/* redirect to login.php */
+		header("Location: login.php");
+		exit;
+		?>
+		
+	<div id="footer"></div>
+</body>
+</html>
