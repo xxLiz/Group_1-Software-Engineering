f299c82a1c393bd747891c45a174cee5740095f1
gilberto-guerra <143899782+gilberto-guerra@users.noreply.github.com>
2024-05-08 19:27:41 -0500

    header.php, not header.html

--- menu2.php
+++ menu2.php
@@ -40,7 +40,7 @@ try {
         </script>
     <script>
         $(function () {
-            $("#header").load("header.html");
+            $("#header").load("header.php");
             $("#footer").load("footer.html");
         });
     </script>
